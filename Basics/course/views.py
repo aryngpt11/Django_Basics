e850027ff78f60d8acaa4f308a78121526195fa0
aryngpt11 <aryan11gs@gmail.com>
2024-06-07 22:46:25 +0530

    settings

--- Basics/course/views.py
+++ Basics/course/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def learn_django(request):
+    return HttpResponse("Hello Django")
+
+def learn_var(request):
+    return HttpResponse("<h1>Hello Django</h1>")
+
+
