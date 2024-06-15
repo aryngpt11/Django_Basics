d6fc987e59586ed7adac87c1eb233df48bb25991
aryngpt11 <aryan11gs@gmail.com>
2024-06-15 15:37:22 +0530

    Templates Inheritance

--- tempinherit/tempinherit/urls.py
+++ tempinherit/tempinherit/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import *
+from course.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',home,name='home'),
+    path('about/',about,name='about'),
+    path('info/',info,name='info')
 ]
