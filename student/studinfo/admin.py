5d6e284c3a4e6cffef72e697a3d37ab9b55cc2dc
aryngpt11 <aryan11gs@gmail.com>
2024-06-30 20:02:50 +0530

    admin register

--- student/studinfo/admin.py
+++ student/studinfo/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from .models import Student
 # Register your models here.
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display=['id','stuid','stuname','stuemail','stupass']
+admin.site.register(Student)
--- student/studinfo/models.py
+++ student/studinfo/models.py
@@ -6,3 +6,5 @@ class Student(models.Model):
     stuname=models.CharField(max_length=50)
     stuemail=models.EmailField(max_length=70)
     stupass=models.CharField(max_length=70)
+    def __str__(self):
+        return self.stuemail
