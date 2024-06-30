08f9323c5083401d1b294dc629c0604f0830fa0e
aryngpt11 <aryan11gs@gmail.com>
2024-06-30 19:25:26 +0530

    all method

--- student/student/urls.py
+++ student/student/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from studinfo.views import studentinfo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',studentinfo,name='studentinfo')
 ]
--- student/studinfo/views.py
+++ student/studinfo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from studinfo.models import Student
 # Create your views here.
+def studentinfo(request):
+    stu=Student.objects.all()
+    return render(request, 'studinfo/studetails.html', {'stu':stu})
