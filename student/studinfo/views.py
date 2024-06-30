64d5ce1bf1d51149975d80c5eb86e9dde157342e
aryngpt11 <aryan11gs@gmail.com>
2024-06-30 19:47:43 +0530

    get method

--- student/studinfo/views.py
+++ student/studinfo/views.py
@@ -3,4 +3,5 @@ from studinfo.models import Student
 # Create your views here.
 def studentinfo(request):
     stu=Student.objects.all()
+    #stu=Student.objects.get(pk=2)
     return render(request, 'studinfo/studetails.html', {'stu':stu})
