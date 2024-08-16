7d6b83f63d395ea8b173a8b33f34fd4a31f22b6d
aryngpt11 <aryan11gs@gmail.com>
2024-08-16 11:44:32 +0530

    form class

---
+++ DjangoFormAPI/enroll/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from enroll.forms import StudentRegistration
+# Create your views here.
+def showformdata(request):
+    fm=StudentRegistration()
+    return render(request,'userregistration.html',{'form':fm})
